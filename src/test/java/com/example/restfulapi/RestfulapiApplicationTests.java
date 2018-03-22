5379a476fcac7fff8d558eacfa657cbda7cdca99
pmorah <pmora@simpartners.com>
2018-03-22 12:46:33 -0300

    App update

--- src/test/java/com/example/restfulapi/RestfulapiApplicationTests.java
+++ src/test/java/com/example/restfulapi/RestfulapiApplicationTests.java
@@ -1,16 +1,29 @@
 package com.example.restfulapi;
 
+import com.example.restfulapi.bean.Car;
+import com.example.restfulapi.repositories.CarRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestfulapiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	CarRepository carRepo;
 
+	static final int quantity = 3;
+
+	@Test
+	public void checkLength() {
+		List<Car> carLength = carRepo.findAll();
+
+		assertEquals(quantity + 1, carLength.size());
+	}
 }
